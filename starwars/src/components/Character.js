01cfbbd03e3b76f580a6d45438890fafe3dce34a
unknown <guivecchi12@gmail.com>
2020-05-01 17:57:54 -0600

    beginning of project

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from "react";
+
+
+function character(props){
+    
+    console.log(props);
+    
+    return(
+        <div>hey</div>
+    )
+}
+
+export default CharacterData;
